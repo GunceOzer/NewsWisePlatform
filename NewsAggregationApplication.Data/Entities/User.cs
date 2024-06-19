73fb49eeee922c72bebcca7cc801c091082aa1d7
gunceozer <ozer.guncedemirel@gmail.com>
2024-06-19 19:00:18 +0200

    Updated user entity

--- NewsAggregationApplication.Data/Entities/User.cs
+++ NewsAggregationApplication.Data/Entities/User.cs
@@ -5,6 +5,10 @@ namespace NewsAggregationApplication.Data.Entities;
 public class User:IdentityUser<Guid>
 {
     public string FullName { get; set; }
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiryTime { get; set; }
+    public DateTime? LastLoginDate { get; set; }
+    public DateTime? LastNotifiedDate { get; set; }
     public ICollection<Comment> Comments { get; set; }
     public ICollection<Like> Likes { get; set; }
     public ICollection<Bookmark> Bookmarks { get; set; }
